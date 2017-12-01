0946f2df21a347e073ca43073bd294191fed019d
JacquesCeChirac <nicolas.cherridi@gmail.com>
2017-12-01 16:29:22 +0100

    livraison

--- index.js
+++ index.js
@@ -10,22 +10,110 @@ var __extends = (this && this.__extends) || (function () {
 })();
 var Animal = (function () {
     // Constructeur qui prend en argument les caractéristiques
-    function Animal(theName, theFoodHabbit, theQuantity, theFoodDescription) {
+    function Animal(theType, theName, theFoodHabbit, theQuantity, theFoodDescription) {
+        this.type = theType;
         this.name = theName;
         this.foodHabbit = theFoodHabbit;
         this.quantity = theQuantity;
         this.foodDescription = theFoodDescription;
+        this.costFood = 1;
     }
+    Animal.prototype.display = function () {
+        if (this.foodHabbit.toLowerCase() === "carnivore") {
+            this.costFood = 10;
+        }
+        var costTotal = this.quantity * this.costFood;
+        console.log("L'animal est un " + this.type);
+        console.log("Et s'appelle " + this.name);
+        console.log("Son régime est un " + this.foodHabbit);
+        console.log("Il lui faut " + this.quantity + " kg de nourriture par jour");
+        console.log("Cela représente " + costTotal + "€ de budget par jour");
+        console.log("");
+    };
     return Animal;
 }());
-var Tigre = (function (_super) {
-    __extends(Tigre, _super);
-    function Tigre(theDetail) {
-        var _this = _super.call(this, "Tigre", "Carnivore", "5kg", "viande") || this;
+var TigreOuLion = (function (_super) {
+    __extends(TigreOuLion, _super);
+    function TigreOuLion(theType, theName, theFoodHabbit, theQuantity, theFoodDescription, theDetail) {
+        var _this = _super.call(this, theType, theName, theFoodHabbit, theQuantity, theFoodHabbit) || this;
         _this.detail = theDetail;
         return _this;
     }
-    return Tigre;
+    return TigreOuLion;
 }(Animal));
-var tigrou = new Tigre(18);
-console.log(tigrou);
+var Zoo = (function () {
+    function Zoo(theName) {
+        this.nbAnimaux = [];
+        this.name = theName;
+    }
+    Zoo.prototype.addAnimal = function (verif, nomAnimal, typeAnimal, quantite, food, detail) {
+        //let type = typeAnimal;
+        var animal;
+        if (verif.toLowerCase() === "tigre" || verif.toLowerCase() === "lion") {
+            animal = new TigreOuLion(verif, nomAnimal, typeAnimal, quantite, food, detail);
+        }
+        else {
+            animal = new Animal(verif, nomAnimal, typeAnimal, quantite, food);
+        }
+        animal.display();
+        this.nbAnimaux.push(animal);
+    };
+    Zoo.prototype.deleteAnimal = function (nom) {
+        var compteur = 0;
+        for (var _i = 0, _a = this.nbAnimaux; _i < _a.length; _i++) {
+            var entry = _a[_i];
+            if (entry.name.toLowerCase() === nom.toLowerCase()) {
+                delete this.nbAnimaux[compteur];
+            }
+            compteur++;
+        }
+    };
+    Zoo.prototype.countAnimal = function () {
+        var compteur = 0;
+        for (var _i = 0, _a = this.nbAnimaux; _i < _a.length; _i++) {
+            var entry = _a[_i];
+            if (entry != undefined) {
+                compteur++;
+            }
+        }
+        console.log("Voici le nombre d'animaux :" + compteur);
+        console.log("");
+    };
+    Zoo.prototype.listAnimals = function () {
+        console.log("Voici la liste des animaux :");
+        for (var _i = 0, _a = this.nbAnimaux; _i < _a.length; _i++) {
+            var entry = _a[_i];
+            if (entry != undefined) {
+                console.log(entry.name);
+            }
+        }
+        console.log("");
+    };
+    Zoo.prototype.totalCost = function () {
+        var cost = 0;
+        for (var _i = 0, _a = this.nbAnimaux; _i < _a.length; _i++) {
+            var entry = _a[_i];
+            if (entry != undefined) {
+                cost += (entry.costFood * entry.quantity);
+            }
+        }
+        console.log("Le coût total en nourriture est de " + cost + " € par jour");
+        console.log("");
+    };
+    Zoo.prototype.display = function () {
+        //
+    };
+    return Zoo;
+}());
+var zoo = new Zoo("Zooland");
+zoo.display();
+zoo.addAnimal("Tigre", "Tigrou", "Carnivore", 4, "viande", "18 rayures");
+zoo.addAnimal("Lion", "Roi Lion", "Carnivore", 3, "viande", "25 cm de crinière");
+zoo.addAnimal("Orang-outan", "Monsieur singe", "végétarien", 5, "fruits");
+zoo.countAnimal();
+zoo.totalCost();
+zoo.listAnimals();
+zoo.deleteAnimal("Tigrou");
+zoo.countAnimal();
+zoo.listAnimals();
+zoo.totalCost();
--- index.ts
+++ index.ts
@@ -1,29 +1,113 @@
 
 class Animal {
-
     // Les caractéristiques de l'animal
+    private type: string;
     private name: string;
     private foodHabbit: string;
-    private quantity: string;
+    private quantity: number;
     private foodDescription: string;
-
+    private costFood : number;
     // Constructeur qui prend en argument les caractéristiques
-    constructor(theName: string, theFoodHabbit: string, theQuantity : string, theFoodDescription : string) {
+    constructor(theType: string, theName: string, theFoodHabbit: string, theQuantity: number, theFoodDescription: string) {
+        this.type = theType;
         this.name = theName;
         this.foodHabbit = theFoodHabbit;
         this.quantity = theQuantity;
         this.foodDescription = theFoodDescription;
+        this.costFood = 1;
+    }
+
+    public display () {
+        if (this.foodHabbit.toLowerCase()==="carnivore"){
+            this.costFood =10;
+        }
+        let costTotal = this.quantity*this.costFood;
+        console.log("L'animal est un "+this.type);
+        console.log("Et s'appelle "+this.name);
+        console.log("Son régime est un "+this.foodHabbit);
+        console.log("Il lui faut "+this.quantity+" kg de nourriture par jour");
+        console.log("Cela représente "+costTotal+"€ de budget par jour");
+        console.log("");
     }
 }
 
-class Tigre extends Animal {
-    private detail: number;
-    constructor(theDetail: number) {
-        super("Tigre","Carnivore","5kg","viande");
+class TigreOuLion extends Animal {
+    private detail: string;
+    constructor(theType: string, theName: string, theFoodHabbit: string, theQuantity : number, theFoodDescription : string, theDetail: string) {
+        super(theType, theName, theFoodHabbit, theQuantity, theFoodHabbit);
         this.detail = theDetail;
     }
 }
 
-let tigrou = new Tigre(18);
+class Zoo{
+    private name: string;
+    nbAnimaux =[];
+    constructor(theName: string) {
+        this.name = theName;
+    }
+    public addAnimal(verif,nomAnimal,typeAnimal,quantite,food,detail?){
+     //let type = typeAnimal;
+    let animal;
+        if (verif.toLowerCase() === "tigre" || verif.toLowerCase() === "lion"){
+            animal = new TigreOuLion(verif,nomAnimal,typeAnimal,quantite,food,detail);
+        } else {
+            animal = new Animal(verif,nomAnimal,typeAnimal,quantite,food);
+        }
+        animal.display();
+        this.nbAnimaux.push(animal);
+    }
+    public deleteAnimal(nom){
+        let compteur: number= 0;
+        for (let entry of this.nbAnimaux) {
+            if (entry.name.toLowerCase() === nom.toLowerCase()){
+            delete this.nbAnimaux[compteur];
+            }
+            compteur++;
+        }
+    }
+    public countAnimal(){
+        let compteur : number = 0;
+        for (let entry of this.nbAnimaux) {
+            if(entry != undefined){
+                compteur++;
+            }
+        }
+        console.log("Voici le nombre d'animaux :"+ compteur);
+        console.log("");
+    }
+    public listAnimals(){
+        console.log("Voici la liste des animaux :")
+        for (let entry of this.nbAnimaux) {
+            if(entry != undefined){
+                console.log(entry.name);
+            }
+        }
+        console.log("");
+    }
+    public totalCost(){
+        let cost : number = 0;
+        for (let entry of this.nbAnimaux) {
+            if(entry != undefined) {
+                cost += (entry.costFood * entry.quantity);
+            }
+        }
+        console.log("Le coût total en nourriture est de "+cost+" € par jour");
+        console.log("");
+    }
+    public display(){
+        //
+    }
+}
 
-console.log(tigrou);
+let zoo = new Zoo("Zooland");
+zoo.display();
+zoo.addAnimal("Tigre","Tigrou","Carnivore", 4,"viande","18 rayures");
+zoo.addAnimal("Lion","Roi Lion","Carnivore", 3,"viande","25 cm de crinière");
+zoo.addAnimal("Orang-outan","Monsieur singe","végétarien", 5,"fruits");
+zoo.countAnimal();
+zoo.totalCost();
+zoo.listAnimals();
+zoo.deleteAnimal("Tigrou");
+zoo.countAnimal();
+zoo.listAnimals();
+zoo.totalCost();
